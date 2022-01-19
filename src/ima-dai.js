005f01b0686078fb5477eb1641243d5d9b0cae37
JonathanTGold <jonathan.gold@86@gmail.com>
2022-01-19 13:14:07 +0200

    fix(FEC-11757): V3: live with Ad: UI wrong state (Video is paused) after closing the new opened Ad tab

--- src/ima-dai.js
+++ src/ima-dai.js
@@ -443,6 +443,7 @@ class ImaDAI extends BasePlugin implements IAdsControllerProvider, IEngineDecora
     this._streamManager.addEventListener(this._sdk.api.StreamEvent.Type.THIRD_QUARTILE, () => this._onAdThirdQuartile());
     this._streamManager.addEventListener(this._sdk.api.StreamEvent.Type.COMPLETE, () => this._onAdCompleteFromSDK());
     this._streamManager.addEventListener(this._sdk.api.StreamEvent.Type.CLICK, () => this._onAdClickFromSDK());
+    this._streamManager.addEventListener(this._sdk.api.StreamEvent.Type.PAUSED, () => this._onAdPause());
   }
 
   _requestVODStream(): void {
@@ -565,7 +566,14 @@ class ImaDAI extends BasePlugin implements IAdsControllerProvider, IEngineDecora
         this.player.pause();
         this.pauseAd();
       }
+    } else if (this._state === ImaDAIState.PAUSED) {
+      this.player.play();
+      this.resumeAd();
     }
+  }
+
+  _onAdPause() {
+    this.pauseAd();
   }
 
   _onAdBreakEnded(): void {
